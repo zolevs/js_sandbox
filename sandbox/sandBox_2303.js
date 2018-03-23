15c1ffa1a7d53f7874759335d1985634ca59abb1
Zoran Ivanovic <zolevs@gmail.com>
2018-03-23 23:47:33 +0100

    fun.

--- sandbox/sandBox_2303.js
+++ sandbox/sandBox_2303.js
@@ -1,7 +1,9 @@
 
-		function fun(){
 			// document.write("Hello again.")
 			// console.log("Hello console.");
-			alert("Wellcome");
+			// alert("Wellcome");
+		function fun(x,y){
+
+			document.write("Opis za x: " + x +"<br/>" + "Opis za y je: " + y);
 		}
-		// fun();
+		fun("This is text"," opis");
