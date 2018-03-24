1273dd6a5fb0f26ae0a8be18f91accec6043a40b
Zoran Ivanovic <zolevs@gmail.com>
2018-03-24 20:52:26 +0100

    decision

--- sandbox/sandBox_2303.js
+++ sandbox/sandBox_2303.js
@@ -4,8 +4,82 @@
 //
 
 
+var number1 = 22;
+var number2 = 22;
+
+if(number1 >= number2){
+	document.write("Heloo " + number1);
+} else {
+	document.write ("Again " + number2);
+}
 
 
+
+
+// var a = 17;
+// var broj = a % 3;
+
+// document.write(broj+"<br/>");
+// document.write(broj + 2 + "<br/>");
+// document.write(broj / 2);
+
+// broj++;
+// console.log(broj);
+
+// broj--;
+// console.log(broj);
+
+// var ime = "Zoran";
+// var prezime = "Ivanovic";
+
+// if ( ( ime == "Zorann") || ( prezime == "Ivanovicc" )){
+// 	console.log(ime);
+// 	console.log(prezime);
+// 	document.write ("Hello !!!!");
+// } else {
+// 	document.write ("Lose");
+// }
+
+// if ((ime == "Zoran") && (prezime == "Ivanovic")){
+// 	document.write ("Hello !!!");
+// }
+
+
+
+// var brojJedan = "Zorann";
+// var brojDva = "Ivanovicc";
+
+// if (brojJedan == "Zoran"){
+// 	if (brojDva == "Ivanovic"){
+// 		document.write ("Odlicno.")
+// 	} else {
+// 		document.write ("Nije bas odlicno.");
+// 	}
+
+// } else {
+// 	document.write ("Nevalja, nista.");
+// }
+
+// var brojJedan = 1;
+// var brojDva = 1;
+
+// if (brojJedan == brojDva){
+// 	document.write ("Ovo je TRUE.")
+// } else {
+// 	document.write ("Ovo je FALSE")
+// }
+
+
+
+// var brojJedan = 29;
+// var brojDva = 28;
+
+// if (brojJedan <= brojDva){
+// 	//
+// 	document.write ("Ovo je TRUE");
+// }
+
+// 	document.write ("Ovo je ipak FALSE.");
 
 // var broj = 25;
 
