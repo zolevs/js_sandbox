4f1d021bad693da20356067aa79683fd55bec131
Zoran Ivanovic <zolevs@gmail.com>
2018-06-04 20:32:21 +0200

    events

--- sandbox/sandBox_0406.js
+++ sandbox/sandBox_0406.js
@@ -1,11 +1,41 @@
-function changeColorOnMouseOver() {
-	var control = document.getElementById("btn");
-	control.style.background = "red";
-	control.style.color = "yellow";
-}
-function changeColorOnMouseOut(){
-	var control = document.getElementById("btn");
-	console.log(control);
-	control.style.background = "black";
-	control.style.color = "white";
-}
+//// 04.06
+//
+//
+//
+
+// // deo 3
+
+
+
+
+
+
+
+// // deo 2
+// function confirmOnSubmit(){
+// 	if(confirm("Are you sure, to submit")) {
+// 		console.log("Submited");
+// 		return true;
+// 	} else {
+// 		console.log("You selected CANCEL");
+// 		return false;
+// 	}
+// }
+
+
+
+
+
+
+// // deo 1
+// function changeColorOnMouseOver() {
+// 	var control = document.getElementById("btn");
+// 	control.style.background = "red";
+// 	control.style.color = "yellow";
+// }
+// function changeColorOnMouseOut(){
+// 	var control = document.getElementById("btn");
+// 	console.log(control);
+// 	control.style.background = "black";
+// 	control.style.color = "white";
+// }
