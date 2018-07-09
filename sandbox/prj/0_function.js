46c475c19baec146e593d66f79aca24246e1ae01
Zoran Ivanovic <zolevs@gmail.com>
2018-07-09 21:27:07 +0200

    just writing

---
+++ sandbox/prj/0_function.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(a,b){
+    return a + b;
+}
+var sum = addTwoNumbers(10,12);
+console.log(sum);
+
+
+/* function sayGreeting(name){
+    //
+    console.log("Hello " + name);
+}
+sayGreeting('Zoran'); 
+*/
