bdf1ae44b2336f69085bd584ce002479680cb870
Zoran Ivanovic <zolevs@gmail.com>
2018-07-09 21:29:53 +0200

    some comments

--- sandbox/prj/0_function.js
+++ sandbox/prj/0_function.js
@@ -1,5 +1,10 @@
 function addTwoNumbers(a,b){
     return a + b;
+
+    // posle ovoga nista ne radi
+
+    console.log("This line doesn't print");
+
 }
 var sum = addTwoNumbers(10,12);
 console.log(sum);
