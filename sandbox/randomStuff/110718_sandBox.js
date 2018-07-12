2e5b79735f02adf64e86a2f0af6acbb02c3eca01
Zoran Ivanovic <zolevs@gmail.com>
2018-07-12 20:03:48 +0200

    array

---
+++ sandbox/randomStuff/110718_sandBox.js
@@ -0,0 +1,56 @@
+var personnel = new Array();
+
+personnel[0] = new Array();
+    personnel[0][0] = "Name0";
+    personnel[0][1] = "Age0";
+    personnel[0][2] = "Address0";
+
+personnel[1] = new Array();
+    personnel[1][0] = "Name1";
+    personnel[1][1] = "Age1";
+    personnel[1][2] = "Address1"
+
+personnel[2] = new Array();
+    personnel[2][0] = "Name2";
+    personnel[2][1] = "Age2";
+    personnel[2][2] = "Address2";
+
+/* var myArray = new Array();
+myArray [0] = "Zoran";
+myArray [1] = "Prezime";
+myArray [2] = "Adresa";
+myArray [3] = 73;
+
+document.write("myArray[0] = " + myArray[0] + "</br>");
+document.write("myArray[1] = " + myArray[1] + "</br>");
+document.write("myArray[2] = " + myArray[2] + "</br>");
+document.write("myArray[3] = " + myArray[3] + "</br>");
+
+myArray[3] = 101;
+document.write("Number is changed myArray[3] " + myArray[3]);
+
+ */
+
+
+
+
+
+/* var degFahren = prompt ('Enter the degrees in Fahrenheit', 75);
+var degCent;
+
+degCent = 5/9 * (degFahren - 32);
+console.log(degCent);
+ */
+
+/* let number = prompt ("enter number with string","");
+let myFloat;
+console.log(parseInt(number));
+
+myFloat = prompt ("Enter float number with string ...");
+myFloat = parseFloat(myFloat);
+
+console.log(myFloat);
+
+*/
+
+
