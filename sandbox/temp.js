f11e3e876891916e4c44332d8c5e85a4c988909e
Zoran Ivanovic <zolevs@gmail.com>
2018-07-19 21:58:02 +0200

    error

--- sandbox/temp.js
+++ sandbox/temp.js
@@ -1,16 +1,11 @@
+function hello (name){
+    console.log('---------');
+    console.log('Hello ' + name);
+    console.log('---------');
+}
+// hello();
 
-let a = [4, 8, 10, 24, 245];
-let b = ['Zoran', 'Eddie', 'David'];
-console.log(b);
-console.log(b[2]);
-
-
-let c = [4, true, 'Zoran'];
-
-console.log(c);
-
-console.log(a.length);
-
-a.push(124);
+let a = hello;
 
 console.log(a);
+a('Zole');
