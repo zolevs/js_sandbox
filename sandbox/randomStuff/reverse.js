23e9cadcc48da4c4982f03ad8d95711339fe4906
Zoran Ivanovic <zolevs@gmail.com>
2018-07-23 21:47:06 +0200

    reversed

---
+++ sandbox/randomStuff/reverse.js
@@ -0,0 +1,17 @@
+function reverseWords(string){
+    var wordsArr = string.split(' '); 
+    var revesedWordsArr = [];
+
+    wordsArr.forEach(element => {
+        var reversedWord = '';
+        for (var i=element.length - 1; i >= 0; i--){
+            reversedWord += element[i];
+        }
+        revesedWordsArr.push(reversedWord);
+    });
+    return revesedWordsArr.join(' ');
+    // console.log(wordsArr);
+}
+
+console.log(reverseWords('Zoran Ivanovic'));
+
