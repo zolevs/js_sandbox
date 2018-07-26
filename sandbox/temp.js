b6557e476cc8bdcb65019a82417fe3a6f894f851
Zoran Ivanovic <zolevs@gmail.com>
2018-07-26 21:06:00 +0200

    just for fun

--- sandbox/temp.js
+++ sandbox/temp.js
@@ -1,3 +1,34 @@
-a = 4;
-message = (a > 2) ? 'true naravno' : 'false svakako';
-console.log(message);
+/* for (let i=0; i < 10; i++){
+    console.log(i);
+}
+
+ */
+
+
+ /* let a = [1, 3, 5, 63, 67, 29];
+
+ for (i = 0; i < a.length-1; i++){
+     console.log(a[i]);
+ }
+  */
+/* 
+let a = [33, 52, 1, 3, 5, 63, 67, 29];
+
+  for (let i = 0; i < a.length; i++) {
+      const element = a[i];
+      console.log(element);
+  }
+   */
+//   console.log(element);
+
+let x = 1;
+while (x < 10){
+    console.log(x);
+    x++;
+    if(x == 3){
+        console.log("Skip");
+        continue;
+    }
+    
+}
+
