7fc958a284154d7809f99e1f067d5ba56c677f0d
Zoran Ivanovic <zolevs@gmail.com>
2018-07-29 23:15:36 +0200

    random

--- sandbox/temp.js
+++ sandbox/temp.js
@@ -1,27 +1,48 @@
-/* function one(){
-    return 'one';
-}
-
-let value = one;
- */
-// console.log(typeof(value));
-// // console.log(one());
-// console.log(value);
-
-// console.log(value());
-
-/* function two(){
-    return function (){
-        console.log('Two');
-    };
-}
-let myFunction = two();
-myFunction(); */
-
-function three(){
-    return function(){
-        return 'three';
+// // Object
+/* 
+let car = {
+    make: 'bmw',
+    model: 'c1',
+    year: 2018,
+    getPrice: function(){
+        // some operation and result is 5000
+        return 5000;
+    },
+    printDescription: function(){
+        console.log(this.make + ' '+ this.model);
     }
 }
+car.printDescription();
+console.log(car.year);
+ */
+// // console.log(car['year']);
+// // console.log(car[1]); // undefined
 
-console.log(three()());
+/*
+var anotherCar = {};
+
+anotherCar.whatever = 'bob';
+console.log(anotherCar.whatever);
+ */
+
+/* var a = {
+    myProperty: {b: 'Hi'}
+}
+console.log(a.myProperty.b); */
+/* 
+var c = {
+    myProperty: [
+        { d: 'one'},
+        { e: 'two'},
+        { f: 'three'},
+        { g: 'last'}
+    ]
+};
+ */
+
+let carLot = [
+    { year: 2017, make: 'Toyota', model: 'runner'},
+    { year: 2016, make: 'Opel', model: 'Corsa'},
+    { year: 2015, make: 'Citroen', model: 'C4'}
+]
+
