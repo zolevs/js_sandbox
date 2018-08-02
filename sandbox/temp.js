09f3dd674a3bcb5d54e76fbea81b627df835226d
Zoran Ivanovic <zolevs@gmail.com>
2018-08-02 23:22:44 +0200

    random

--- sandbox/temp.js
+++ sandbox/temp.js
@@ -1,13 +1,17 @@
-function sayHello(name){
-    return function(){
-        console.log('howdy ' + name);
-    }
+var start = "A".charCodeAt();
+var array = [];
+
+for (var i = start; i < start + 26; i++){
+    array.push(String.fromCharCode(i));
 }
 
-let zozon = sayHello('Zole');
-let bob = sayHello('bob');
-let miki = sayHello('miki');
+console.log(array);
+var randomArray = [];
 
-zozon();
-bob();
-miki();
+for (var i in array){
+    var pos = Math.round(Math.random()*randomArray.length);
+    randomArray.splice(pos, 0, array[i]);
+    // console.log(pos);
+}
+
+console.log(randomArray);
