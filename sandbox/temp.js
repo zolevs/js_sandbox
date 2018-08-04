8bef3c769cd82d769cf8ddf9bc1c5552634be52a
Zoran Ivanovic <zolevs@gmail.com>
2018-08-04 15:42:16 +0200

    random

--- sandbox/temp.js
+++ sandbox/temp.js
@@ -1,17 +1,16 @@
-var start = "A".charCodeAt();
+var start = "a".charCodeAt();
 var array = [];
 
-for (var i = start; i < start + 26; i++){
+for (var i = start; i < start+26; i++){
     array.push(String.fromCharCode(i));
 }
-
 console.log(array);
-var randomArray = [];
 
+var  randomArray = [];
 for (var i in array){
-    var pos = Math.round(Math.random()*randomArray.length);
-    randomArray.splice(pos, 0, array[i]);
-    // console.log(pos);
+    var position = Math.round(Math.random()*randomArray.length);
+    randomArray.splice(position, 0, array[i]);
+    
 }
 
 console.log(randomArray);
