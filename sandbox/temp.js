a682575e2debbe6f85de54e5a7f4601da3236d88
Zoran Ivanovic <zolevs@gmail.com>
2018-08-07 22:54:40 +0200

    book

--- sandbox/temp.js
+++ sandbox/temp.js
@@ -1,4 +1,5 @@
-(function(){
+/*
+ (function(){
     var message = "Hello message";
     function printMessage(){
         console.log(message);
@@ -11,3 +12,27 @@
     };
     return obj;
 }) ();
+ */
+
+var theObject = (function(){
+    var message = "Hello message";
+    // console.log('Hello from terminal...');
+
+    function printMessage(){
+        console.log(message);
+    }
+    // printMessage();
+
+    var obj = {};
+
+    obj.probni = function(){
+        printMessage();
+    }
+    return obj;
+}) ();
+
+
+theObject.probni();
+
+
+
