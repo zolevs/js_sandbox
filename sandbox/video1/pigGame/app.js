6d153a89f6cbe55cc2f236921a3c311e94b5edac
Zoran Ivanovic <zolevs@gmail.com>
2018-08-22 19:36:42 +0200

    stage

--- sandbox/video1/pigGame/app.js
+++ sandbox/video1/pigGame/app.js
@@ -8,3 +8,43 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-'+activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// var x = document.querySelector('#score-0').textContent;
+// x.dice;
+// console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+/*
+ function btn(){
+    console.log('addEventListener');
+}
+// btn();
+*/
+
+// document.querySelector('.btn-roll').addEventListener('click', btn);
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    // console.log("Some");
+    // // random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + ".png";
+
+});
