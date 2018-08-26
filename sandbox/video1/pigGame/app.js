8ea1337db100dfb0d9fee9e8ab827370ccd0d47b
Zoran Ivanovic <zolevs@gmail.com>
2018-08-26 23:20:10 +0200

    new game

--- sandbox/video1/pigGame/app.js
+++ sandbox/video1/pigGame/app.js
@@ -9,10 +9,7 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 // dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
@@ -24,12 +21,6 @@ activePlayer = 0;
 // x.dice;
 // console.log(x);
 
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 /*
  function btn(){
@@ -90,3 +81,29 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         // ako je 1 onda sklanjamo jedan sliku
         document.querySelector('.dice').style.display = 'none';
     }
+
+    document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+        scores = [0, 0];
+        activePlayer = 0;
+        roundScore = 0;
+
+        document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+// document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+
+document.querySelector('.player-0-panel').classList.add('active');
+};
