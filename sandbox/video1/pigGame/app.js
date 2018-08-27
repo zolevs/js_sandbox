7036d0b25f2aee8b3c30719982f3dca4b3fa5c70
Zoran Ivanovic <zolevs@gmail.com>
2018-08-27 18:55:21 +0200

    gamePlaying condiotional

--- sandbox/video1/pigGame/app.js
+++ sandbox/video1/pigGame/app.js
@@ -8,8 +8,9 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 init();
+
 
 // dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice);
@@ -32,12 +33,14 @@ init();
 // document.querySelector('.btn-roll').addEventListener('click', btn);
 document.querySelector('.btn-roll').addEventListener('click', function () {
     // console.log("Some");
+    if(gamePlaying){
+
     // // random number
     var dice = Math.floor(Math.random() * 6) + 1;
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + ".png";
-   
+    
     if (dice !== 1){
         // add score
         roundScore += dice;
@@ -48,25 +51,31 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         nextPlayer();
     }
 
+    }
+
 });
 
     document.querySelector('.btn-hold').addEventListener('click', function (){
-        // add current score to global score
-        scores[activePlayer] += roundScore;
-        // updatee the UI
-        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-        
-        // check if the player won the game
-        if (scores[activePlayer] >= 20) {
-            document.querySelector('#name-'+activePlayer).textContent = 'Winner';
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
-            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
-        } else {
-            nextPlayer(); // // // !!! proveri ishod ako promenimoo gde se nalazi kao recimo pre if-a    
-        }
-        
+        if(gamePlaying){
 
+            // add current score to global score
+            scores[activePlayer] += roundScore;
+            // updatee the UI
+            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+            
+            // check if the player won the game
+            if (scores[activePlayer] >= 20) {
+                document.querySelector('#name-'+activePlayer).textContent = 'Winner';
+                document.querySelector('.dice').style.display = 'none';
+                document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+                document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+
+                gamePlaying = false;
+            } else {
+                nextPlayer(); // // // !!! proveri ishod ako promenimoo gde se nalazi kao recimo pre if-a    
+            }
+
+        }
 
     });
 
@@ -85,25 +94,26 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     document.querySelector('.btn-new').addEventListener('click', init);
 
 function init(){
-        scores = [0, 0];
-        activePlayer = 0;
-        roundScore = 0;
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
 
-        document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
-document.getElementById('name-0').textContent = 'Player 1';
-document.getElementById('name-1').textContent = 'Player 2';
-// document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
-document.querySelector('.player-0-panel').classList.remove('winner');
-document.querySelector('.player-1-panel').classList.remove('winner');
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    // document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
 
-document.querySelector('.player-0-panel').classList.remove('active');
-document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
 
-document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 };
