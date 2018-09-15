2a294add9a4b7c751af19aea15240a1868aa81a6
Zoran Ivanovic <zolevs@gmail.com>
2018-09-15 21:26:53 +0200

    template literals

---
+++ sandbox/templateLiterals.js
@@ -0,0 +1,8 @@
+const name = 'Zoran';
+const lastName = 'Ivanovic';
+const age = 44;
+const phrase = "My name is "+name+" "+lastName+". And I'm " + age + " years old";
+
+console.log(phrase);
+
+const div = document.createElement("div");
