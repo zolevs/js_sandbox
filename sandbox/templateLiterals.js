f657969482fc6b3b662122ff5382852161dae096
Zoran Ivanovic <zolevs@gmail.com>
2018-09-16 12:53:47 +0200

    className and classList

--- sandbox/templateLiterals.js
+++
@@ -1,8 +0,0 @@
-const name = 'Zoran';
-const lastName = 'Ivanovic';
-const age = 44;
-const phrase = "My name is "+name+" "+lastName+". And I'm " + age + " years old";
-
-console.log(phrase);
-
-const div = document.createElement("div");
