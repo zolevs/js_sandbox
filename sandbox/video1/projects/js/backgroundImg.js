441f8838cae8a6fd20ccd9c5f9e407610fcfd239
zolevs <zolevs@gmail.com>
2018-11-05 23:35:35 +0100

    image slider

--- sandbox/video1/projects/js/backgroundImg.js
+++ sandbox/video1/projects/js/backgroundImg.js
@@ -7,4 +7,43 @@
         "contBcg-3",
         "contBcg-4"
     ];
+
+    // setup the counter
+    let counter = 0;
+
+    // select buttons
+
+    const btn = document.querySelectorAll('.btn');
+    console.log(btn);
+
+    btn.forEach(function(btn) {
+        btn.addEventListener('click', function(event){
+            // get event target
+            let value = event.target;
+            // console.log(value);
+
+            if (value.classList.contains('btn-left')) {
+                counter--;
+                if(counter < 0){
+                    counter = pictures.length - 1;
+                }
+                console.log(counter);
+                document.querySelector('.img-container').style.backgroundImage = `url('img/${pictures[counter]}.jpeg')`;
+            }
+
+            if (value.classList.contains('btn-right')) {
+                counter++;
+                if(counter > pictures.length - 1){
+                    counter = 0;
+                }
+                console.log(counter);
+                document.querySelector('.img-container').style.backgroundImage = `url('img/${pictures[counter]}.jpeg')`;
+            }
+
+        });
+    });
+       
+    
+
+
 })();
