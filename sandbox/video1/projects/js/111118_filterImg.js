6026b6d5e6fc9a9baca4cf6fb667fdfe16c0ece5
zolevs <zolevs@gmail.com>
2018-11-12 18:51:29 +0100

    search

--- sandbox/video1/projects/js/111118_filterImg.js
+++ sandbox/video1/projects/js/111118_filterImg.js
@@ -34,7 +34,41 @@
 
 // search input
 (function(){
+// target serch box
+const search = document.getElementById('search-item');
 
+search.addEventListener('keyup', function(){
+    let value = search.value.toLowerCase().trim();
+    // console.log(value);
+   
+    // 
 
+    const items = document.querySelectorAll('.store-item');
+    items.forEach(function(item){
+        let type = item.dataset.item;
+        // console.log(type);
+        // console.log(typeof type);
 
+        /*         if(type.includes(value)){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        } */
+
+        let length = value.length;
+        // console.log(length);
+        let match = type.slice(0,length);
+     
+        // console.log(value);
+        // console.log(match);
+        
+        if(value === match){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+
+        
+    });
+});
 })();
