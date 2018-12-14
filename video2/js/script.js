626ba1b71a42fe58512edaa5cbf95bf11b05ea26
zolevs <zolevs@gmail.com>
2018-12-14 23:17:11 +0100

    event object

--- video2/js/script.js
+++ video2/js/script.js
@@ -10,10 +10,38 @@ var h2 = document.querySelector('header h2');
 //     console.log('Mouseover');
 // }
 
-function a(){
+/* function a(){
     console.log('clicked');
 }
 function b(){
     console.log('mouse over');
+}
+
+ */
+
+/* h2.addEventListener('click', function(){
+    console.log('Click');
+}); */
+/* h2.addEventListener('click', a);
+h2.addEventListener('click', b);
+
+function a(){
+    console.log('Clicked a...');
+}
+function b(){
+    console.log('Clicked b...')
+}
+ */
+
+// // event object
+
+h2.addEventListener('click', a);
+
+function a(e){
+    console.log('Clicked a...');
+    console.log(e);
+    console.log(e.clientX);
+    console.log(e.target);
+    console.log(this);
 }
 
