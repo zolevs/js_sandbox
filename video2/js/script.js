6f7490f93405a3c8eb60985bcb2bec4991151cde
zolevs <zolevs@gmail.com>
2018-12-15 22:34:45 +0100

    dom navigation

--- video2/js/script.js
+++ video2/js/script.js
@@ -1,23 +1,3 @@
-// // get & set attribute
+var listItem = document.getElementById('list-item');
 
-// var div = document.querySelector('div.wrapper');
-// // div.getAttribute('class');
-
-// // // setAttribute('attr', 'value');
-
-// div.setAttribute('style', 'background: coral');
-
-// div.removeAttribute('style');
-// div.hasAttribute('style');
-
-
-var btn = document.getElementById('add-btn');
-var input = document.getElementById('add-input');
-
-btn.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('hello');
-    input.setAttribute('type','submit');
-    input.setAttribute('value',input.value);
-
-});
+// listItem.parentElement.style.background = 'green';
