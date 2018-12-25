9d8936ef6bd74723292448e4cc428237108ef1c8
Zoran Ivanovic <zolevs@gmail.com>
2018-12-25 23:45:54 +0100

    video

---
+++ sandbox/video3/251218_index.js
@@ -0,0 +1,9 @@
+let city = 'Vrsac';
+let country = 'Vojvodina';
+let location = 'Serbia';
+
+let myLocation = `${city} ${country} ${location}`;
+let myLocation2 = city + ' ' + country + ' '+ location;
+console.log(myLocation);
+
+console.log(myLocation2);
