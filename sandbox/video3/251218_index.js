79e322d82df0c606233c595477cf30ea13f8147f
Zoran Ivanovic <zolevs@gmail.com>
2018-12-27 23:37:46 +0100

    variable

--- sandbox/video3/251218_index.js
+++ sandbox/video3/251218_index.js
@@ -1,8 +1,5 @@
-// numbers
+// // variables
+let petName = 'Oskar';
+petName = 'Spot';
 
-let num = '10' * 2;
-let multiply = 4.2 / 2;
-
-
-console.log(num);
-console.log(multiply);
+console.log(petName);
