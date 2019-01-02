9800ff8a943d1894055b48e0bf802b45100bb57e
Zoran Ivanovic <zolevs@gmail.com>
2019-01-02 22:30:55 +0100

    undefined

---
+++ sandbox/video3/010119_index.js
@@ -0,0 +1,74 @@
+let name;
+console.log(name);
+
+name = 'jane';
+
+if (name === undefined) {
+    console.log('Provider a name');
+    
+} else {
+    console.log(name)
+}
+
+
+let square = function (num) {
+    console.log(num)
+}
+let result = square(44);
+console.log(result);
+
+//
+
+let age = 44;
+age = undefined;
+console.log(age);
+
+
+
+// function - input(argument) code output(return value)
+
+/* let greetUser = function () {
+    console.log('Welcome user');
+}
+
+greetUser();
+
+let square = function (num){
+    console.log(num);
+    let result = num * num;
+    return result;
+}
+
+// let num = 3
+// square(num);
+let value = square (3);
+let otherValue = square(10);
+console.log(value);
+console.log(otherValue); */
+
+// // //
+
+
+/* let convertFahrenheitToCelsius = functin (fahrenheit){
+    let convertedToCelsius = (fahrenheit - 32) * 5/9;
+    return convertedToCelsius;
+} */
+/* 
+function convertFahrenheitToCelsius(fahrenheit) {
+    let convertedToCelsius = (fahrenheit - 32) * 5/9;
+    return convertedToCelsius;
+} */
+
+// let converted = convertFahrenheitToCelsius(32);
+// console.log(converted);
+/* 
+let convertFahrenheitToCelsius = function (fahrenheit){
+    let converted = (fahrenheit - 32) * 5/9;
+    return converted;
+}
+
+console.log(convertFahrenheitToCelsius(32));
+console.log(convertFahrenheitToCelsius(68));
+ */
+
+
