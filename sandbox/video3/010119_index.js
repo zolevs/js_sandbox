95ef9781ebc8951efbaaf95e7031afa5a12ebbaa
Zoran Ivanovic <zolevs@gmail.com>
2019-01-03 18:38:35 +0100

    fn arguments, fn scope

--- sandbox/video3/010119_index.js
+++ sandbox/video3/010119_index.js
@@ -1,4 +1,59 @@
-let name;
+// // // function scope start 
+
+// global scope (convertFahrenheitToCelsius, tempOne)
+    // local sope (fahrenheit, celsius)
+        // local sope (isFreezing)
+
+let convertFahrenheitToCelsius = function (fahrenheit){
+    let celsius = (fahrenheit - 32) * 5 / 9;
+    if(celsius < 0){
+        let isFreezing = true;
+    }
+    return celsius;
+}
+let tempOne = convertFahrenheitToCelsius(32);
+
+console.log(tempOne);
+
+// // // function scope end
+
+
+// arguments start
+
+/* 
+let add = function(a, b, c){
+    return a - b + c;
+}
+
+let result = add(10, 2, 10);
+console.log(result);
+
+// // default values (arguments)
+let getScoreText = function (name = 'Anonymous', score = 0){
+    // console.log(name);
+    // console.log(score);
+    return "Name: " + name + " Score: " + score;
+}
+
+let scoreText = getScoreText( undefined, 92);
+console.log(scoreText);
+
+
+// // // //
+// total tipPercent = .2
+
+let tip = function (total = 0, tipPercent = .10){
+    return tipAmount = total * tipPercent;
+}
+
+let tipResult = tip(100, .3);
+console.log(tipResult);
+ */
+
+// arguments end
+
+
+/* let name;
 console.log(name);
 
 name = 'jane';
@@ -23,7 +78,7 @@ let age = 44;
 age = undefined;
 console.log(age);
 
-
+ */
 
 // function - input(argument) code output(return value)
 
