903b2dd4c39daa2f7a27116919ae68f3aa09adc6
Zoran Ivanovic <zolevs@gmail.com>
2019-01-15 19:31:11 +0100

    string method

--- sandbox/video3/130119_index.js
+++ sandbox/video3/130119_index.js
@@ -1,4 +1,4 @@
-
+/* 
 let name = 'Oliver Nedeljkovic';
 console.log(name.length);
 
@@ -6,8 +6,63 @@ console.log(name.toUpperCase());
 console.log(name.toLowerCase());
 
 console.log(name.includes('era')); // true or false
+console.log(name.includes('de'));
+
+let password = 'abc123password888';
+console.log(password.includes('password123'));
+
+// // Trim
+
+let lastName = ' Ipakk ';
+console.log(lastName.length);
+lastName = lastName.trim();
+console.log(lastName.length);
+
+// // isValidPassword
+
+let passwordToCheck = ' newMemeberOfGroup '
+passwordToCheck = passwordToCheck.trim();
+let contain = passwordToCheck.includes('password');
+
+if (passwordToCheck.length >= 8 && contain !== true ){
+     console.log('true');
+} */
+
+// // //
+// // ver 1
+/* function isValidPassword(toCheck){
+    // console.log(toCheck);
+    if(toCheck.length >= 8) {
+        // console.log('proslo')
+        return toCheck.includes('password');
+    } else {
+        return "Doesn't meat the criteria. ";
+    }
+}
+ */
 
 
+// // ver 2
+ 
+/* let isValidPassword = function(password){
+    if(password.length > 8 && !password.includes('password')){
+        return true;
+    }  else {
+        return false;
+    }
+} 
+ */
+// // ver 3
+let isValidPassword = function (password){
+    return password.length > 8 && !password.includes('password');
+}
+
+ 
+
+console.log(isValidPassword('asdd'))
+console.log(isValidPassword('abc123'))
+console.log(isValidPassword('asdfgpassword'));
+console.log(isValidPassword('asdfgpasssword'));
 
 // // // // // // //
 
