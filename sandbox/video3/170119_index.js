d49c77d5f4428b8d789033c192f2f8e0d3670066
Zoran Ivanovic <zolevs@gmail.com>
2019-01-17 21:03:17 +0100

    const and variables

---
+++ sandbox/video3/170119_index.js
@@ -0,0 +1,13 @@
+/* 
+const isRaining = true;
+isRaining = false;
+console.log(isRaining);
+ */
+
+const person = {
+    age: 45
+}
+// person = {}; // // not allowed
+console.log(person);
+person.age = 46;
+console.log(person);
