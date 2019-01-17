db7eca5092dda0ec4ac69172d5b132c98eaa2702
Zoran Ivanovic <zolevs@gmail.com>
2019-01-17 22:20:15 +0100

    just test var

--- sandbox/video3/170119_index.js
+++ sandbox/video3/170119_index.js
@@ -4,10 +4,28 @@ isRaining = false;
 console.log(isRaining);
  */
 
-const person = {
+/* const person = {
     age: 45
 }
 // person = {}; // // not allowed
 console.log(person);
 person.age = 46;
 console.log(person);
+
+ */
+
+/* 
+ var firstName = 'Zozon';
+ firstName = 'New Name';
+ console.log(firstName);
+
+ var firstName = 'New with var';
+
+ console.log(firstName);
+ */
+
+if (10 === 10){
+    let first = "Name";
+}
+
+console.log(first);
