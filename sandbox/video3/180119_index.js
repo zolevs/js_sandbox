1f3074f10da644c2e09418e051164583073819d4
Zoran Ivanovic <zolevs@gmail.com>
2019-01-19 23:22:37 +0100

    array

---
+++ sandbox/video3/180119_index.js
@@ -0,0 +1,9 @@
+
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+console.log(notes[1]);
+console.log(notes[notes.length - 1]);
+
+const toDoList = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5'];
+
+console.log(`You have ${toDoList.length} things on your todo list`);
+console.log(`Las one is ${toDoList[toDoList.length - 1]}`)
