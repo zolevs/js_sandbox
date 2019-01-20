c19616bd8f57a53a6781ce8b8d792c6c821942d2
Zoran Ivanovic <zolevs@gmail.com>
2019-01-20 19:59:41 +0100

    array foreach

--- sandbox/video3/180119_index.js
+++ sandbox/video3/180119_index.js
@@ -1,17 +1,55 @@
+
 /* 
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
 console.log(notes[1]);
 console.log(notes[notes.length - 1]);
  */
-/* const toDoList = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5'];
+const toDoList = ['Note 1', 'Note 2', 'Note 3', 'Note 4', 'Note 5'];
 
 console.log(`You have ${toDoList.length} things on your todo list`);
-console.log(`Las one is ${toDoList[toDoList.length - 1]}`)
+console.log(`Last one is ${toDoList[toDoList.length - 1]}`);
+console.log(toDoList);
+
+toDoList[2] = 'This is now the new note 3';
+
+console.log('--------------------');
+/* const exampleCall = function(value, index){
+    console.log(index, value);
+}
+toDoList.forEach(exampleCall);
  */
 
-const notes = ['Note 1', 'Note 2', 'Note 3'];
+toDoList.forEach(function (item, index){
+    // console.log(index, item);
+    console.log(`This is number ${index} and todo is ${item} `);
+});
 
+
+
+
+
+// // // //
+
+// console.log(toDoList);
+
+/* 
+console.log('----------------');
+
+toDoList.splice(2, 1);
+console.log(toDoList);
+
+removed = toDoList.pop();
+console.log(removed);
+
+toDoList.push('Notes Push');
+console.log(toDoList);
+ */
+
+// // //
+
+// const notes = ['Note 0', 'Note 1', 'Note 2'];
+/* 
 notes.push('Note XX');
 let removed = notes.pop();
 
@@ -22,4 +60,11 @@ console.log(notes.unshift());
 console.log(notes.length);
 console.log(notes);
 
+ */
 
+// notes.splice(0, 1,'Note XX');
+
+/* notes[1] = 'New';
+
+console.log(notes.length);
+console.log(notes); */
