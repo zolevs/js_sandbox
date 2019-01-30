3a0c9c3cb56ad4d1d681226533d5fba9a3e39016
Zoran Ivanovic <zolevs@gmail.com>
2019-01-30 23:35:36 +0100

    fun

---
+++ sandbox/video3/function/300119_index.js
@@ -0,0 +1,24 @@
+let name = 'Zole';
+if( name === undefined){
+    console.log('Please provide a name')
+} else {
+    console.log(name);
+}
+
+console.log(name);
+
+// 
+let square = function (num){
+    console.log(num);
+} 
+
+let result = square();
+console.log(result);
+
+// //
+
+let age = 44;
+age = null;
+console.log(age);
+
+
