160b915c8b826123caf935739039410c91965a79
Zoran Ivanovic <zolevs@gmail.com>
2019-02-04 23:59:08 +0100

    array

---
+++ sandbox/video3/arrays/040219_notes.js
@@ -0,0 +1,4 @@
+const notes = ['Notes 1','Notes 2','Notes 3', 'Notes 4', 'Notes 5'];
+console.log(notes.length);
+console.log(notes[2]);
+console.log(notes[notes.length-1]);
---
+++ sandbox/video3/arrays/040219_todo.js
@@ -0,0 +1,7 @@
+const todos = ['Buy milk', 'Wash hand', 'Exercise', 'Eat healthy', 'Read JS'];
+console.log(`You have ${todos.length} todos on your list.`);
+
+// console.log(`First on todo list is ${todos[0]} and last on todo list is ${todos[todos.length - 1]}`);
+
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`);
