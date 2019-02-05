4a07bd55bbc5b7ee87c644d6e61240cc8384595f
Zoran Ivanovic <zolevs@gmail.com>
2019-02-05 22:24:20 +0100

    forEach

--- sandbox/video3/arrays/040219_notes.js
+++ sandbox/video3/arrays/040219_notes.js
@@ -1,4 +1,40 @@
 const notes = ['Notes 1','Notes 2','Notes 3', 'Notes 4', 'Notes 5'];
 console.log(notes.length);
-console.log(notes[2]);
-console.log(notes[notes.length-1]);
+/* 
+notes.push('New note');
+console.log(notes.length);
+
+let removed = notes.pop();
+console.log(`Removed item from array: ${removed}`);
+
+let removeFirst = notes.shift();
+console.log(removeFirst);
+
+notes.unshift('Add at the beginning');
+console.log(notes[0]);
+console.log(notes);
+ */
+// notes.splice(notes.length, 0, 'Zole');
+// console.log(notes);
+
+// notes.splice(1, 0, 'This is the new item');
+// console.log(notes);
+
+// // // //
+
+/* const doThis = function(){
+
+};
+
+notes.forEach(doThis);
+
+// //
+
+notes.forEach(function(){
+
+}) */
+
+notes.forEach(function(item, index){
+    console.log('Test');
+    console.log(item, index);
+})
