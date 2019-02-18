4d4212c7ec53d62faf3b273ae28c1a65e9a57bd2
Zoran Ivanovic <zolevs@gmail.com>
2019-02-18 19:40:12 +0100

    search array

---
+++ sandbox/video3/arrays/180219_searchArray.js
@@ -0,0 +1,47 @@
+const myTodos = ['Buy bread', 'Go to gym', 'Record video'];
+
+console.log(myTodos.indexOf('Go to gym'));
+// console.log(myTodos[myTodos.indexOf('Record video')]);
+
+const newTodos = [{
+    title: 'Buy Bread',
+    isDone: false,
+}, {
+    title: 'Go to gym',
+    isDone: false,
+}, {
+    title: 'Record video',
+    isDone: true,
+}];
+
+/* const index = newTodos.findIndex(function(todo, index){
+    console.log(todo);
+    return todo.title === 'Go to gym';
+});
+
+console.log(index); */
+
+// // // Method 1
+/* 
+    const findTodo = function(myTodos, title){
+    const index = myTodos.findIndex(function(todo, index){
+        return todo.title.toLowerCase() === title.toLowerCase()
+    });
+    return myTodos[index];
+}
+
+let printMe = findTodo(newTodos, 'Go to gym');
+console.log(printMe);
+ */
+
+ // // // Method 2
+
+ const findTodo = function(myTodos, title){
+    const titleReturned = myTodos.find(function(todo, index){
+        return todo.title.toLowerCase() === title.toLowerCase();
+    });
+    return titleReturned
+ }
+
+ let printMe = findTodo(newTodos, 'Go to gym');
+ console.log(printMe);
