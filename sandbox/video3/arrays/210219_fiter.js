a6ee9b4f17b014000806f6b81ad1448344640ec6
Zoran Ivanovic <zolevs@gmail.com>
2019-02-21 22:41:42 +0100

    chaining

--- sandbox/video3/arrays/210219_fiter.js
+++ sandbox/video3/arrays/210219_fiter.js
@@ -18,7 +18,7 @@
 
 
 // // map on array
-
+/* 
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let product = function(value){
@@ -29,4 +29,39 @@ let square = nums.map(product);
 console.log(square);
 
 let quad = nums.map(product).map(product);
-console.log(quad);
+console.log(quad); */
+
+/* let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let obj = nums.map(function(value, index, arr){
+    // console.log(value, index, arr);
+    return {
+        index: index,
+        value: value,
+        square: value * 2,
+        cube: value * 3,
+        originalArr: arr,
+    };
+});
+
+console.log(obj);
+
+
+ */
+
+// //// chaining
+
+let str = "   Heare is a string.";
+
+let str1 = str.replace('Heare', 'Here');
+
+let str2 = str1.toUpperCase();
+
+let str3 = str2.trim();
+
+console.log(str3);
+
+// // with chaining
+console.log(str.replace('Heare', 'Here').toUpperCase().trim().split(" ").join('-'));
+
+// // // 
+
