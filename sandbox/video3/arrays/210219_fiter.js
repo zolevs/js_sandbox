0e347e1fe74ca9d1b7333fe1b4ad2b6712b3fb26
Zoran Ivanovic <zolevs@gmail.com>
2019-02-21 20:32:25 +0100

    map

--- sandbox/video3/arrays/210219_fiter.js
+++ sandbox/video3/arrays/210219_fiter.js
@@ -1,15 +1,32 @@
 // //   filtering an array
 // //   
 
-let scores = [84, 34, 24, 99, 28, 38, 5, 8, 11, 0];
-/* 
-let passScores = scores.filter(function (value, index) {
-    // console.log(`value: ${value} - ${index}`);
-    return value > 60;
-});
- */
-// // arrow funcition
-let passScores = scores.filter( value => value > 60)
+// let scores = [84, 34, 24, 99, 28, 38, 5, 8, 11, 0];
+// /* 
+// let passScores = scores.filter(function (value, index) {
+//     // console.log(`value: ${value} - ${index}`);
+//     return value > 60;
+// });
+//  */
+// // // arrow funcition
+// let passScores = scores.filter( value => value > 60)
 
-console.log(scores);
-console.log(passScores);
+// console.log(scores);
+// console.log(passScores);
+
+
+
+
+// // map on array
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let product = function(value){
+    return value * value;
+};
+
+let square = nums.map(product);
+console.log(square);
+
+let quad = nums.map(product).map(product);
+console.log(quad);
