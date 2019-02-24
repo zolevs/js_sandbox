60e6279e40fce410e9fc58ea15085d908928d62f
Zoran Ivanovic <zolevs@gmail.com>
2019-02-24 23:35:37 +0100

    array

---
+++ sandbox/video3/arrays/240219_arrayRefresh.js
@@ -0,0 +1,25 @@
+let characters = ['Luke', 'Leia', 'Han', 'Zozon', 'Ralph'];
+/* 
+characters.splice(1, 1, 'New Item', 'New item 2');
+
+console.log(characters);
+
+characters.push('BB8');
+characters.push('Outlowe');
+
+characters.reverse();
+console.log(characters);
+
+characters.sort();
+console.log(characters);
+characters.reverse().sort();
+console.log(characters);
+
+ */
+
+ let position = characters.indexOf('Han');
+
+ console.log(position);
+
+ let lastPosition = characters.lastIndexOf('Han');
+ console.log(lastPosition);
