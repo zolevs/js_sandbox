96453228a2de405bc85766e4b6c36f8ac10556ae
Zoran Ivanovic <zolevs@gmail.com>
2019-02-26 23:15:02 +0100

    dom

--- sandbox/video3/dom/notes/notes-app.js
+++ sandbox/video3/dom/notes/notes-app.js
@@ -1,1 +1,23 @@
+
 console.log('This is from different file.');
+console.log('Test live server');
+
+// // DOM - Document Object Model
+
+let p = document.querySelectorAll('p');
+
+console.log(p);
+const ps = document.querySelectorAll('p');
+
+ps.forEach(function (p){
+    console.log(p.textContent);
+    // p.remove();
+    p.textContent = '******';
+})
+
+let text = p.textContent;
+console.log(text);
+
+
+let h1 = document.querySelector('h2');
+h1.remove();
