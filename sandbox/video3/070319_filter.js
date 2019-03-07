a6eb01a00321781e70ccd676be226e7056f3bd58
Zoran Ivanovic <zolevs@gmail.com>
2019-03-07 23:29:52 +0100

    filter

---
+++ sandbox/video3/070319_filter.js
@@ -0,0 +1,16 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let evenNumbers = numbers.filter(function(n){
+//     // // even numbers
+//     // return n % 2 == 0;
+
+//     // // odd number
+//     return !(n % 2 == 0);
+// });
+
+// // arrow function
+
+// let evenNumbers = numbers.filter( n => !(n % 2 == 0));
+
+// console.log(evenNumbers);
+
