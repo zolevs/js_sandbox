0daae3b0c917559b729cdc2f49783c51ddaafa3f
Zoran Ivanovic <zolevs@gmail.com>
2019-03-12 23:39:29 +0100

    todo filter

--- sandbox/video3/dom/todo-app/120319_todo.js
+++ sandbox/video3/dom/todo-app/120319_todo.js
@@ -1,26 +1,43 @@
-const notes = [{
-    title: 'My next trip',
-    body: 'I would like to go to Spain',    
-},{
-    title: 'Habbits to work on',
-    body: 'Exercise',
-},{
-    title: 'Office modification',
-    body: 'Get a new seat'
-},{
-    title: 'Have a walk',
-    body: 'Get a bycicle'
-}];
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
 
+// // 
 
-const ps = document.querySelectorAll('p');
-
-ps.forEach(function(p){
-    p.textContent = '****';
+const incompleteTodos = todos.filter(function(todo){
+    return !(todo.completed)
 });
 
-// add new element
-const newParagraph = document.createElement('p');
-newParagraph.textContent = 'New text from JS';
-// console.log(newParagraph);
-document.querySelector('body').appendChild(newParagraph);
+const summary = document.createElement('h2');
+summary.textContent = `You have ${incompleteTodos.length} todos left.`;
+console.log(incompleteTodos);
+document.querySelector('body').appendChild(summary);
+
+todos.forEach(function(todo){
+    const p = document.createElement('p');
+    p.textContent = todo.text;
+    document.querySelector('body').appendChild(p);
+})
+
+
+// const ps = document.querySelectorAll('p');
+
+// ps.forEach(function(p){
+//     p.textContent = '****';
+// });
+
+// // add new element
+// const newParagraph = document.createElement('p');
+// newParagraph.textContent = 'New text from JS';
+// // console.log(newParagraph);
+// document.querySelector('body').appendChild(newParagraph);
