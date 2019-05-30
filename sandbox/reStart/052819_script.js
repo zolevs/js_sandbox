65e940e6b4723bcd76946d8c53ef252ecb0f8d2a
zolevs <zolevs@gmail.com>
2019-05-30 23:01:31 +0200

    numbers

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,22 +1,36 @@
-// let ID = Symbol();
-let ID = Symbol('This is unique ID');
-console.log(ID);
-console.log(typeof ID);
+let points = 1;
 
-let SOME_KEY =Symbol();
-let obj = {};
+// points = points + 10;
+points += 10;
+console.log(points);
 
-obj[SOME_KEY] = 123;
-console.log(obj[SOME_KEY]);
+let score = 2;
+score++;
+console.log(score);
 
-console.log(typeof 123);
-console.log(typeof 1.232);
+score--;
+console.log(score);
 
-console.log(Number.isInteger(10.2));
-console.log(Number.isInteger(21));
+console.log('2' * 5); //
+console.log('2'+5); //
 
-let someNumber = new Number(3);
-console.log(someNumber);
+console.log('11') // string
+console.log(+'11') //converted to number
 
-let anotherNumber = 12;
+console.log(10..toString()) // convert to string
+let n = 10;
+console.log(n.toString()); // convert variable to string
 
+
+let n1 = 102.2354;
+let n2 = 23.4325552;
+
+console.log(n1.toFixed(2));
+
+console.log(Number.isInteger(12.3));
+console.log(Number.isInteger(12));
+
+console.log(parseInt('10'));
+console.log(parseInt(10.66));
+
+console.log(parseFloat(10.66));
