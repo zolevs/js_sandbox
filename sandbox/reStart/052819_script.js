6614dd0316d7fc4df2a66f76a388e8e181f57e86
zolevs <zolevs@gmail.com>
2019-05-31 23:39:26 +0200

    numbers

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,36 +1,6 @@
-let points = 1;
-
-// points = points + 10;
-points += 10;
-console.log(points);
-
-let score = 2;
-score++;
-console.log(score);
-
-score--;
-console.log(score);
-
-console.log('2' * 5); //
-console.log('2'+5); //
-
-console.log('11') // string
-console.log(+'11') //converted to number
-
-console.log(10..toString()) // convert to string
-let n = 10;
-console.log(n.toString()); // convert variable to string
-
-
-let n1 = 102.2354;
-let n2 = 23.4325552;
-
-console.log(n1.toFixed(2));
-
-console.log(Number.isInteger(12.3));
-console.log(Number.isInteger(12));
-
-console.log(parseInt('10'));
-console.log(parseInt(10.66));
-
-console.log(parseFloat(10.66));
+let n = 123;
+console.log(n == 3);
+console.log(n == 123);
+console.log(n == '123');
+console.log(n === '123');
+console.log(n === 123);
