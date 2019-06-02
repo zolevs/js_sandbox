aef96bc15d6ce21039a6e9bdc674dae07fc9e9ba
zolevs <zolevs@gmail.com>
2019-06-03 00:06:10 +0200

    array

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,11 +1,28 @@
-// loops
+// // array
 
-let n = 10;
-while(n > 0){
-    console.log(n);
-    n--;
-}
-let n2 = 10;
-do{
-    console.log(n2)
-} while (n2 > 20)
+let myArray = [];
+
+let anotherArray = new Array();
+
+console.log(myArray);
+
+let fruits = ['apple', 'orange', 'grape'];
+
+console.log(fruits.length);
+console.log(fruits[1]);
+
+console.log(fruits[fruits.length-1]);
+
+fruits[0] = 'banana';
+
+console.log(fruits[0]);
+
+fruits[6] = 'limun';
+console.log(fruits);
+
+let [x,y,z] = [1,2,3];
+console.log(x,z);
+
+let multiArray = [[1,2,3],['a','b','c']];
+console.log(multiArray[0][2]);
+console.log(multiArray[1][1]);
