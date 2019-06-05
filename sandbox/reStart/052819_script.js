ebf8d261bde4ce66380f7790c76b6e02fec82dde
zolevs <zolevs@gmail.com>
2019-06-05 20:44:28 +0200

    methods

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,14 +1,42 @@
-// // loop over array
+// // array methods
 
-let colors = ['red','gren','blue','orange'];
-for (let i = 0; i<colors.length; i++){
-    console.log(colors[i]);
-}
+let numbers = ['one', 'two', 'three', 'four', 'five'];
 
-for (let color of colors){
-    console.log(color);
-}
+// console.log(numbers.pop());
+// console.log(numbers.shift());
 
-colors.forEach(element => {
-    console.log(element);
-});
+// console.log(numbers);
+
+// console.log(numbers.push('add'));
+
+// console.log(numbers);
+
+// console.log(numbers.unshift('front'));
+// console.log(numbers);
+
+// console.log(numbers.concat(['concat 1', 'concat 2']));
+// console.log(numbers); // arrary is unchanged with concat
+// numbers = numbers.concat(['second 2', 'second 3']);
+
+// console.log(numbers);
+
+// numbers = [...numbers, ...['spread 1', 'spread 2']];
+// console.log(numbers);
+
+// console.log(numbers.join('; '));
+
+// console.log(numbers.slice(2, 4));
+
+console.log(numbers.splice(1,1));
+console.log(numbers);
+
+console.log(numbers.reverse());
+
+console.log(numbers.sort());
+console.log([1, 3, 10].sort()); // // !!!!!
+
+console.log(numbers.indexOf('two'));
+console.log(numbers.includes('one'));
+
+let splitString = 'how are you ?';
+console.log(splitString.split(' '));
