312310318e29a7a39d8cf469b8d604d53dec9eb2
zolevs <zolevs@gmail.com>
2019-06-08 23:43:23 +0200

    maps and function

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,21 +1,18 @@
-let numbers = new Set();
-numbers.add(1);
+// // function
 
-console.log(numbers);
-numbers.add(2);
-console.log(numbers);
-
-
-numbers.add(1);
-console.log(numbers);
-
-let fruits = new Set(['apples', 'oranges', 'pinapple', 'apples']); 
-console.log(fruits); // // no duplicates allowed
-
-let veggies = new Set(['potato', 'tomato', 'carrots']);
-let veggiesArray = [...veggies];
-console.log(veggiesArray);
-
-for (let fruit of fruits){
-    console.log(fruit);
+function hello(){
+    console.log('Hello world');
 }
+
+hello();
+
+let color = function(){
+    console.log('red');
+}
+color();
+
+let fruit = function apples(){
+    console.log('apples');
+}
+
+fruit();
