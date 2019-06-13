a3babdbcc7ee7c74e29b00c3394933aa2eaf5b72
zolevs <zolevs@gmail.com>
2019-06-13 23:34:29 +0200

    theory

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -15,13 +15,27 @@
 
 // ask("Do you agree?", showOk, showCancel);
 
-function ask(question, yes, no){
-    if (confirm(question)) yes()
-    else no();
-}
+// function ask(question, yes, no){
+//     if (confirm(question)) yes()
+//     else no();
+// }
 
-ask(
-    "Do you agree?",
-    function(){alert ("You agreed")},
-    function(){alert ("You canceled the execution")}
-);
+// ask(
+//     "Do you agree?",
+//     function(){alert ("You agreed")},
+//     function(){alert ("You canceled the execution")}
+// );
+
+
+let x = function(){
+    console.log('I am called from inside function');
+};
+
+let y = function(callback){
+    console.log('do something');
+    callback();
+};
+
+y(x);
+
+console.log(x);
