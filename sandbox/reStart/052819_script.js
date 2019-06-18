e8e63d0d81262dfea8225b16192a2ab76fcf2b86
zolevs <zolevs@gmail.com>
2019-06-18 23:00:08 +0200

    obj

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -11,14 +11,64 @@ let john = {
 };
 
 for (let key of Object.keys(john)){
-    console.log(key)
+    console.log(key);
 }
+
 for (let value of Object.values(john)){
     console.log(value);
-};
+}
 
 for (let [key, value] of Object.entries(john)){
-    console.log(key, value);
-};
+    console.log(`Key ${key} Value ${value}`);
+}
 
-// console.log(john);
+john.lastname = "Smith";
+console.log(john.lastname);
+
+john.eyeColor = 'Blue';
+john.work = function(){
+    return 'I program for a living';
+}
+
+console.log(john);
+john.wife = {
+    firstname: 'Mary',
+    age: 35
+}
+
+console.log(john.wife.firstname);
+
+let bob = {
+    program(){
+        console.log('I program');
+        return this;
+    },
+    walk(){
+        console.log('I walk');
+        return this;
+
+    },
+    sleep(){
+        console.log('I sleep');
+        return this;
+
+    }
+}
+
+bob.program().walk().sleep();
+
+let {x, ...remaining} = {x:1, a:2, b:3, c:4};
+console.log(x);
+
+console.log(remaining);
+
+restParam({
+    b:2,
+    a:1,
+    c:3,
+});
+
+function restParam({a, ...x}){
+    console.log(a);
+    console.log(x);
+}
