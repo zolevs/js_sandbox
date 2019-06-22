d925e017fb03ce195fb8b26017b55d1c43e3eb51
zolevs <zolevs@gmail.com>
2019-06-22 21:16:43 +0200

    theory

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,74 +1,61 @@
-// // 
 let john = {
     firstname: 'John',
     lastname: 'Jones',
     age: 45,
     occupation: 'programmer',
-    //walk: walk(){
     walk(){
-        return `I, ${this.firstname} ${this.lastname} am walknig.`;
+        return `I ${this.firstname} ${this.lastname} am walking`
     }
-};
-
-for (let key of Object.keys(john)){
-    console.log(key);
 }
 
+// console.log(john.walk());
+for(let key of Object.keys(john)){
+    console.log(key);
+}
 for (let value of Object.values(john)){
     console.log(value);
 }
 
-for (let [key, value] of Object.entries(john)){
-    console.log(`Key ${key} Value ${value}`);
+for(let [key, value] of Object.entries(john)){
+    console.log(key, value);
 }
-
-john.lastname = "Smith";
+john.lastname = 'Smith';
 console.log(john.lastname);
 
 john.eyeColor = 'Blue';
+console.log(john.eyeColor);
+
 john.work = function(){
     return 'I program for a living';
 }
-
-console.log(john);
-john.wife = {
-    firstname: 'Mary',
-    age: 35
-}
-
-console.log(john.wife.firstname);
 
 let bob = {
     program(){
         console.log('I program');
         return this;
     },
-    walk(){
-        console.log('I walk');
+    eat(){
+        console.log('I eat.');
         return this;
-
-    },
-    sleep(){
-        console.log('I sleep');
-        return this;
-
     }
 }
 
-bob.program().walk().sleep();
+bob.program().eat();
 
-let {x, ...remaining} = {x:1, a:2, b:3, c:4};
+let {x, ...remaining} = {x:1, a:2, c: 3, d: 4}
+
 console.log(x);
-
 console.log(remaining);
 
 restParam({
-    b:2,
     a:1,
+    b:2,
     c:3,
+    d:4
 });
 
-function restParam({a, ...x}){
-    console.log(a);
-    console.log(x);
+function restParam({d, ...x}){
+    console.log(d);
+    console.log(x)
 }
+
