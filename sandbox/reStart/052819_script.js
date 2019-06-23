6dcdb15a8e31319fdf8aace7bbf691a25cc06d21
zolevs <zolevs@gmail.com>
2019-06-23 22:59:29 +0200

    json

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,61 +1,13 @@
-let john = {
-    firstname: 'John',
-    lastname: 'Jones',
+// // JSON
+
+let person = {
+    name: 'John',
     age: 45,
-    occupation: 'programmer',
-    walk(){
-        return `I ${this.firstname} ${this.lastname} am walking`
-    }
+    occupatoin: 'programmer'
 }
 
-// console.log(john.walk());
-for(let key of Object.keys(john)){
-    console.log(key);
-}
-for (let value of Object.values(john)){
-    console.log(value);
-}
+let personJSON = JSON.stringify(person);
+console.log(personJSON);
 
-for(let [key, value] of Object.entries(john)){
-    console.log(key, value);
-}
-john.lastname = 'Smith';
-console.log(john.lastname);
-
-john.eyeColor = 'Blue';
-console.log(john.eyeColor);
-
-john.work = function(){
-    return 'I program for a living';
-}
-
-let bob = {
-    program(){
-        console.log('I program');
-        return this;
-    },
-    eat(){
-        console.log('I eat.');
-        return this;
-    }
-}
-
-bob.program().eat();
-
-let {x, ...remaining} = {x:1, a:2, c: 3, d: 4}
-
-console.log(x);
-console.log(remaining);
-
-restParam({
-    a:1,
-    b:2,
-    c:3,
-    d:4
-});
-
-function restParam({d, ...x}){
-    console.log(d);
-    console.log(x)
-}
-
+let personObject = JSON.parse(personJSON);
+console.log(personObject);
