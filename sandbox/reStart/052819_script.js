ab6c77dcb06a97670b1e4b59e6811859f2786211
zolevs <zolevs@gmail.com>
2019-06-24 23:55:18 +0200

    Math

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,13 +1,17 @@
-// // JSON
+// // math 
 
-let person = {
-    name: 'John',
-    age: 45,
-    occupatoin: 'programmer'
+console.log(Math); // list all math methods
+
+console.log(Math.PI);
+console.log(Math.abs(-123));
+console.log(Math.floor(1.6));
+console.log(Math.ceil(1.6));
+
+console.log(Math.max(1,3,4,5,7,2));
+
+console.log(Math.random());
+console.log(Math.round(Math.random()*5)); // random in a range 0 to 4
+
+for (let i = 1; i <= 10; i++){
+    console.log(Math.round(Math.random()*5)); // random in a range 0 to 4
 }
-
-let personJSON = JSON.stringify(person);
-console.log(personJSON);
-
-let personObject = JSON.parse(personJSON);
-console.log(personObject);
