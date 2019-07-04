3f7ac2670e4748c2ede10bf2967607c718e763b0
zolevs <zolevs@gmail.com>
2019-07-04 23:59:43 +0200

    recursion

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,32 +1,8 @@
-(function(){
+// // recursive funciton
 
-    console.log('Hello from IIFE');
-})();
+function factorial(n){
+    if (n === 0) return 1;
+    return n * factorial(n-1)
+}
 
-(function(){
-    let color = 'red';
-    let language = 'Javascript';
-
-    init();
-
-    function init(){
-       color = 'green';
-       language = 'C#';
-       console.log(`Color ${color} - Language ${language}`);
-    }
-})();
-
-// init();
-
-let result = (function(){
-    return 'Return from IIFE';
-})();
-
-console.log(result);
-
-(function(msg, times){
-    for (let i = 1; i<= times; i++){
-        console.log(msg);
-    }
-})('Hello', 4);
-
+console.log(factorial(3));
