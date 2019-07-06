322fd593d7168e4622addb21535426d34784b601
zolevs <zolevs@gmail.com>
2019-07-06 23:56:51 +0200

    promises

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,16 +1,16 @@
-// // closures
+// // promises
 
-function outer(){
-    let outside = 'outside';
-
-    function inner(){
-        let inside = 'inside';
-
-        console.log(outside);
-        console.log(inside);
-    }
-    console.log(outside);
-    inner();
+function getRandomNumber(n){
+    return Math.floor(Math.random() * n);
 }
 
-outer();
+let food = ['grapes', 'bananas', 'pork', 'beef', 'meat', 'onion', 'chicken'];
+
+let getMeat = new Promise( (resolve, reject) => {
+    let n = getRandomNumber(6);
+    (n>2) ? resolve(n) : reject(n);
+});
+
+getMeat.then(
+    result => console.log(`I'm gona eat ${food[result]}`), 
+    result => console.log(`food result ${food[result]}`))
