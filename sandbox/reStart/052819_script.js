38e2a354d7a5b416ffdce9a33a2e1c51759210ed
zolevs <zolevs@gmail.com>
2019-08-03 23:23:35 +0200

    window document

--- sandbox/reStart/052819_script.js
+++ sandbox/reStart/052819_script.js
@@ -1,39 +1,13 @@
-let clickP = document.querySelector('p#click');
-console.log(clickP);
+// window.alert('Hello');
+// alert('Hello');
 
-clickP.addEventListener('click', () => console.log('Clicked'));
-clickP.addEventListener('mousedown', () => console.log('mouse down'));
-clickP.addEventListener('mouseup', () => console.log('mouse up'));
+// confirm('Are you sure');
 
-let doubleP = document.querySelector('p#dblClick');
-console.log(doubleP);
-
-doubleP.addEventListener('dblclick', highlight);
-function highlight(e){
-    e.target.classList.toggle('highlight');
-}
-let mouse = document.querySelector('p#mouse');
-mouse.addEventListener('mouseenter', highlight);
-mouse.addEventListener('mouseleave', highlight);
-
-// key events
-
-addEventListener('keydown', highlight);
-addEventListener('keypress', (e) => console.log(`You pressed ${e.key}`));
-
-addEventListener('keypress', function(e){
-    if(e === 'Enter'){
-        console.log('enter is pressed');
-    }
-});
-
-let removeP = document.querySelector('p#remove');
-console.log(removeP);
-removeP.addEventListener('click', remove);
-
-function remove(e){
-    console.log('Removed');
-    e.target.style.backgroundColor = 'yellow';
-    removeP.removeEventListener('click', remove);
+if(confirm('Are you sure')){
+    console.log('That is ok');
+} else {
+    console.log('Cancel');
 }
 
+let name = prompt('Your name');
+console.log(name);
