0500ca6a5dde831785ae4be26d6a9d5529ccb62b
zolevs <zolevs@gmail.com>
2019-08-22 23:48:17 +0200

    theory

---
+++ sandbox/reStart/082119_script.js
@@ -0,0 +1,28 @@
+// console.log(true, false, "true", "false");
+
+// let email = 'luigi@thenetninja.co.uk';
+// let numbers = ['one', 'two', 'three', 'four'];
+
+// let result = email.includes('@');
+// console.log(result);
+// result = email.includes('!');
+// console.log(result);
+
+// let contain = numbers.includes('two');
+// console.log(contain);
+
+let age = 25;
+console.log(age == 25);
+console.log(age == 35);
+console.log(age != 35);
+console.log(age != 25);
+console.log(age <= 25);
+console.log(age < 25);
+
+let name = 'shaun';
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+
+
+
+
