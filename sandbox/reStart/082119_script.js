7342c7ec3b6ddd440fbd7ddac4c527cf28023046
zolevs <zolevs@gmail.com>
2019-08-28 21:47:08 +0200

    theory

--- sandbox/reStart/082119_script.js
+++ sandbox/reStart/082119_script.js
@@ -1,40 +1,5 @@
-// const myFunc = (callbackFunc) => {
-    
-//     let value = 50;
-//     callbackFunc(value);
-// };
+let people = ['mario', 'luigi', 'shaun', 'kikiricki'];
+
+people.forEach((person, index) => console.log(`${index} - ${person}`));
 
 
-// myFunc( value => {
-    
-//     console.log(value);
-// });
-
-// let people = ['Mario', 'Dalio', 'Luigi', 'Chin-Lu', 'Donotelo'];
-
-// // people.forEach(function(person, index){
-// //     console.log(person, index);
-// // });
-
-// const logPerson = (person, index) => {
-//     console.log(`${index} - Hello ${person}`)
-// }
-
-// // people.forEach( person => {
-// //     console.log(person);
-// // });
-// people.forEach( logPerson );
-
-const ul = document.querySelector('.people');
-
-const people =  ['Mario', 'Dalio', 'Luigi', 'Chin-Lu', 'Donotelo'];
-let html = ``;
-
-// people.forEach(function (person){
-//     // html template
-//     html += `<li style = "color: #ff00ff;">${person}</li>`;
-// });
-people.forEach ( person => { html += `<li style = "color: #ff00ff;">${person}</li>` });
-ul.innerHTML = html;
-
-console.log(html);
