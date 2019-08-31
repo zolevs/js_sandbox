32b4683fdc5e0119eecd9b96e0bf42cc77489650
zolevs <zolevs@gmail.com>
2019-08-31 23:48:17 +0200

    obj

--- sandbox/reStart/082119_script.js
+++ sandbox/reStart/082119_script.js
@@ -1,3 +1,14 @@
+console.log(Math);
 
-const name = 'Shaun';
+console.log(Math.PI);
+console.log(Math.E);
 
+const area = 7.9278;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.trunc(area));
+
+//
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 10));
