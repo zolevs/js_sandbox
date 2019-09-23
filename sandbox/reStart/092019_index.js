0fd91e948b96a315aa97297898bdba9b2953d7e2
zolevs <zolevs@gmail.com>
2019-09-23 19:24:42 +0200

    todo setup

--- sandbox/reStart/092019_index.js
+++ sandbox/reStart/092019_index.js
@@ -1,5 +1,105 @@
-// reduce
+// // // // 
 
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50}
+  ];
+
+// const filtered = products.filter(product => {
+//     return product.price > 20;
+// });
+
+// const promos = filtered.map(product => {
+//     return `the ${product.name} is ${product.price / 2} pounds`;
+// });
+
+// console.log(promos);
+
+
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product) => `the ${product.name} is ${product.price / 2} pounds`;
+
+
+
+
+
+
+
+
+
+
+
+// // sort method
+
+// const scores = [10, 5, 40, 60, 70, 90, 10, 20, 70];
+
+// scores.sort((a,b) => a - b);
+//   console.log(scores);
+
+// const names = ['mario', 'shaun', 'chin-li', 'luigi', 'abacus'];
+
+// const players = [
+//     {name: 'mario', score: 20},
+//     {name: 'luigi', score: 10},
+//     {name: 'chun-li', score: 50},
+//     {name: 'yoshi', score: 30},
+//     {name: 'shaun', score: 70}
+//   ];
+
+//   names.sort();
+//   names.reverse();
+//   console.log(names);
+
+//   scores.sort();
+//   console.log(scores);
+
+// scores.reverse();
+// console.log(scores);
+
+
+// // // //
+// players.sort((a,b) => {
+//     if(a.score > b.score) {
+//         return -1;
+//     } else if (b.score > a.score){
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+// // // //
+
+// players.sort((a,b) => {
+//     return b.score - a.score;
+// });
+
+// console.log(players);
+
+
+
+
+
+
+
+
+// // find method
+
+// const scores = [10, 5, 0, 40, 60, 70, 90, 10, 20, 70];
+
+// const firstHighScore = scores.find((score) => {
+//     return score > 50;
+// });
+
+// console.log(firstHighScore);
+
+
+
+
+// reduce
 
 // const scores = [10, 20, 60, 40, 70, 90, 30];
 // const result = scores.reduce((acc, curr)=>{
@@ -14,22 +114,22 @@
 
 
 
-const scores = [
-    {player: 'mario', score: 50},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 70},
-    {player: 'crystal', score: 60},
-    {player: 'yoshi', score: 30},
-    {player: 'mario', score: 15},
-    {player: 'crystal', score: 60}
-  ];
+// const scores = [
+//     {player: 'mario', score: 50},
+//     {player: 'yoshi', score: 30},
+//     {player: 'mario', score: 70},
+//     {player: 'crystal', score: 60},
+//     {player: 'yoshi', score: 30},
+//     {player: 'mario', score: 15},
+//     {player: 'crystal', score: 60}
+//   ];
 
-const marioTotal = scores.reduce((acc, curr) => {
-    if(curr.player === 'mario'){
-        acc += curr.score;
-    }
-    return acc;
-}, 0);
+// const marioTotal = scores.reduce((acc, curr) => {
+//     if(curr.player === 'mario'){
+//         acc += curr.score;
+//     }
+//     return acc;
+// }, 0);
 
 
 
