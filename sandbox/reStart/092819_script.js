0b458dfc5ad63994e6e55e063e0a3c0952713875
zolevs <zolevs@gmail.com>
2019-09-29 23:03:21 +0200

    callback

--- sandbox/reStart/092819_script.js
+++ sandbox/reStart/092819_script.js
@@ -1,10 +1,33 @@
-console.log(1);
-console.log(2);
 
-setTimeout(() => {
-    console.log('callback fired')
-}, 2000);
+const getTodos = (callback) => {
 
-console.log(3);
-console.log(4);
-console.log(5);
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange',()=>{
+        // console.log(request, request.readyState);
+        if(request.readyState === 4 && request.status === 200){
+            // console.log(request, request.responseText);
+            callback(undefined, request.responseText);
+        } else if(request.readyState === 4){
+            // console.log('no data');
+            callback('no data', undefined);
+        }
+    });
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
+    request.send();
+
+
+};
+
+getTodos((err, data) => {
+    console.log('callback fired');
+    // console.log(err, data);
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+
+
