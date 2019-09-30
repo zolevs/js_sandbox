023a17cf8f33926134d8dbfee986005f70d9510f
zolevs <zolevs@gmail.com>
2019-09-30 22:39:01 +0200

    promise

--- sandbox/reStart/092819_script.js
+++ sandbox/reStart/092819_script.js
@@ -1,33 +1,63 @@
 
-const getTodos = (callback) => {
+const getTodos = (resources, callback) => {
 
-    const request = new XMLHttpRequest();
-    request.addEventListener('readystatechange',()=>{
-        // console.log(request, request.readyState);
-        if(request.readyState === 4 && request.status === 200){
-            // console.log(request, request.responseText);
-            callback(undefined, request.responseText);
-        } else if(request.readyState === 4){
-            // console.log('no data');
-            callback('no data', undefined);
-        }
+    return new Promise((resolve) => {
+
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange',()=>{
+            // console.log(request, request.readyState);
+            if(request.readyState === 4 && request.status === 200){
+                // console.log(request, request.responseText);
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+                // callback(undefined, data);
+            } else if(request.readyState === 4){
+                // console.log('no data');
+                // callback('no data', undefined);
+                reject('error getting resource');
+            }
+        });
+    
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+        request.open('GET', resources);
+        request.send();
+
+
     });
-
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
-    request.send();
-
-
 };
 
-getTodos((err, data) => {
-    console.log('callback fired');
-    // console.log(err, data);
-    if(err){
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+getTodos('data.json').then((data) => {
+    console.log('promise resolved: ', data);
+}).catch((err) => {
+    console.log('Promise rejected ', err);
 });
 
 
 
+
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//       // fetch
+//         resolve('some data');
+//         reject('some error');
+//     });
+    
+// };
+
+
+
+
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+
+
+
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// })
