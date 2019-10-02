2f338618e6d4ae820747b671ab0277c247e1260f
zolevs <zolevs@gmail.com>
2019-10-02 23:22:34 +0200

    async

--- sandbox/reStart/092819_script.js
+++ sandbox/reStart/092819_script.js
@@ -1,13 +1,41 @@
 // fetch api
 
-fetch('data.json').then((response)=>{
-    console.log('resolved', response);
-    return response.json();
-}).then((data) => {
-    console.log(data);
-}).catch((err) => {
-    console.log('rejected', err);
-});
+const getTodos = async () => {
+
+    const response = await fetch('dataa.json');
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    }
+    // console.log(response);
+    const data = await response.json();
+    // console.log(data);
+    return data;
+};
+
+// const test = getTodos();
+// console.log(test);
+
+// getTodos().then(data => {
+//     console.log('resolved', data);
+// });
+
+console.log(1);
+console.log(2);
+getTodos()
+    .then(data => console.log('resolved', data))
+    .catch(err => console.log('rejected', err.message))
+console.log(3);
+console.log(4);
+
+
+// fetch('data.json').then((response)=>{
+//     console.log('resolved', response);
+//     return response.json();
+// }).then((data) => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.log('rejected', err);
+// });
 
 
 
