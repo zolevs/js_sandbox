e8de584fc041663d1b0f858284f793223bf9a916
zolevs <zolevs@gmail.com>
2019-10-06 23:25:57 +0200

    async

--- sandbox/reStart/092819_script.js
+++ sandbox/reStart/092819_script.js
@@ -1,40 +1,44 @@
-const getTodos = (callback) => {
+const getTodos = (resource, callback) => {
 
 
-const request = new XMLHttpRequest();
-console.log(request);
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200) {
-        // console.log(request.readyState);
-        // console.log(request, request.responseText);
-        callback(undefined, request.responseText);
-    } else if(request.readyState === 4) {
-        // console.log('no data ...');
-        callback('no data', undefined);
-    }
-});
+    const request = new XMLHttpRequest();
+    console.log(request);
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState);
+        if(request.readyState === 4 && request.status === 200) {
+            // console.log(request.readyState);
+            // console.log(request, request.responseText);
+            const data = JSON.parse(request.responseText);
+            // callback(undefined, request.responseText);
+            callback(undefined, data);
+        } else if(request.readyState === 4) {
+            // console.log('no data ...');
+            callback('no data', undefined);
+        }
+    });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/posts/');
-request.send();
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/posts/');
+    // request.open('GET', 'data.json');
+    request.open('GET', resource);
+    request.send();
 
 
 };
 
-console.log(1);
-console.log(2);
 
-
-getTodos((err, data) => {
-    console.log('Callback fired...');
-    // console.log(err, data);
-    if(err){
-        console.log(err);
-    } else {
+getTodos('data.json', (err, data) => {
+    console.log(data);
+    getTodos('data2.json', (err, data) => {
         console.log(data);
-    }
+        getTodos('data3.json', (err, data)=> {
+            console.log(data);
+        });
+    });
+    // console.log(err, data);
+    // if(err){
+    //     console.log(err);
+    // } else {
+    //     console.log(data);
+    // }
 });
 
-
-console.log(3);
-console.log(4);
