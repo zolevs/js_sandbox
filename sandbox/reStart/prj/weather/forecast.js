0edd90612daad1ef4c5c037de70dc9f1dbde049a
zolevs <zolevs@gmail.com>
2019-10-13 22:01:42 +0200

    weather apo

--- sandbox/reStart/prj/weather/forecast.js
+++ sandbox/reStart/prj/weather/forecast.js
@@ -1,8 +1,66 @@
 const key = 'apDVt0BT7ko1GLcwskpg6u2RA95qCANF';
 
-const getCity = async(city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apiKey=${key}&q=${city}`;
 
+// get weather information
+const getWeather = async (id) => {
+  
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+  
+  // get city information
+  const getCity = async (city) => {
+  
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+  
+
+
+
+// // get weather
+// const getWeather = async(id) => {
+//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+//     const query = `${id}?apikey=${key}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+
+// };
+
+
+
+// // get city
+// const getCity = async(city) => {
+//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//     const query = `?apikey=${key}&q=${city}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
     
-}
+// };
+
+// // getCity('manchester').then(data => {
+// //         return getWeather(data.Key);
+// //     }).then(data =>{
+// //         console.log(data);
+// //     })
+// //     .catch(err => console.log(data));
+
+// // getWeather(329260);
