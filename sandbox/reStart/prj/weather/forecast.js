d402b1bee002c32144280f3f6e023ce45e6b5412
zolevs <zolevs@gmail.com>
2019-10-14 23:49:30 +0200

    weather

--- sandbox/reStart/prj/weather/forecast.js
+++ sandbox/reStart/prj/weather/forecast.js
@@ -29,38 +29,3 @@ const getWeather = async (id) => {
   
 
 
-
-// // get weather
-// const getWeather = async(id) => {
-//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-//     const query = `${id}?apikey=${key}`;
-
-//     const response = await fetch(base + query);
-//     const data = await response.json();
-
-//     return data[0];
-
-// };
-
-
-
-// // get city
-// const getCity = async(city) => {
-//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-//     const query = `?apikey=${key}&q=${city}`;
-
-//     const response = await fetch(base + query);
-//     const data = await response.json();
-
-//     return data[0];
-    
-// };
-
-// // getCity('manchester').then(data => {
-// //         return getWeather(data.Key);
-// //     }).then(data =>{
-// //         console.log(data);
-// //     })
-// //     .catch(err => console.log(data));
-
-// // getWeather(329260);
