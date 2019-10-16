60d821fa55ce318d965aaf66d4c4a9754049491c
zolevs <zolevs@gmail.com>
2019-10-16 21:50:46 +0200

    weather

--- sandbox/reStart/prj/weather/app.js
+++ sandbox/reStart/prj/weather/app.js
@@ -1,8 +1,33 @@
 
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+  // console.log(data);
+  // const cityDets = data.cityDets;
+  // const weather = data .weather;
+
+  const {cityDets, weather} = data;
+  console.log(data);
+  // update details template
+
+  details.innerHTML = `
+      <h5 class="my-3">${cityDets.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>
+  `;
+
+  // remove the d-none class if present 
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
+};
 
 const updateCity = async (city) => {
-
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
   return {
@@ -10,8 +35,6 @@ const updateCity = async (city) => {
     // weather: weather
     // isto to samo krace u kreiranju objekta bi bilo
     cityDets, weather
-
-
   };
 
 };
@@ -26,7 +49,7 @@ cityForm.addEventListener('submit', e => {
 
   // update the ui with new city
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
 
