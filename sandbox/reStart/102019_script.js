a96c62adfd431aea18393463e0746608b607ab9f
zolevs <zolevs@gmail.com>
2019-11-03 23:29:02 +0100

    class

--- sandbox/reStart/102019_script.js
+++ sandbox/reStart/102019_script.js
@@ -1,29 +1,53 @@
-const userOne = {
-    username: 'Zoran',
-    email: 'zozon@mail.com',
-    login(){
-        console.log('User logged in');
-    },
-    logout(){
-        console.log('User logged out');
+class User {
+    constructor(username, email){
+        // set up properties
+        this.username = username;
+        this.email = email;
+        this.score = 0;
     }
-};
-
-console.log(userOne.username, userOne.email);
-userOne.login();
-
-
-const userTwo = {
-    username: 'Zozon',
-    email: 'papadubi@gg.com',
+    // // metode ne razdvajamo , zato gore nema , 
     login(){
-        console.log('User logged in');
-    },
-    logout(){
-        console.log('User logged out');
+        console.log(`${this.username} just logged in.`);
+        return this;
     }
-};
+    logout(){
+        console.log(`${this.username} just logged OUT`);
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has score of ${this.score}`);
+        return this;
+    }
+}
 
-console.log(userTwo.username, userTwo.email);
+class Admin extends User{
+    constructor(username, email, title){
+        super(username, email);
+        this.title = title;
+    }
+    deleteUser(user){
+        users = users.filter(u => u.username !== user.username);
+    }
+}
 
-const userThree = new user();
+const userOne = new User('Zozon', 'zozo@mail.com');
+const userTwo = new User('Tama', 'test@mail.com');
+const userThree = new Admin('SubAdmin', 'admin@test.com', 'black-belt');
+
+
+console.log(userOne);
+console.log(userTwo);
+console.log(userThree);
+
+// userTwo.login();
+// userOne.logout();
+
+userOne.login().incScore().incScore().incScore();
+
+let users = [userOne, userTwo, userThree];
+console.log(users);
+
+userThree.deleteUser(userTwo);
+
+console.log(users);
