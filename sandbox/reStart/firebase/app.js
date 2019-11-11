be44080ddb5c499d2bd529ede1f724b8a19b456f
zolevs <zolevs@gmail.com>
2019-11-11 15:12:49 +0100

    firebase

--- sandbox/reStart/firebase/app.js
+++ sandbox/reStart/firebase/app.js
@@ -7,25 +7,48 @@ function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
-    li.setAttribute('data-is', doc.id);
+    li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    // deleting data
+    cross.addEventListener('click', (e)=>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log(id);
+        db.collection('cafes').doc(id).delete();
+    })
 }
 
 // getting data
-db.collection('cafes').get().then((snapshot)=>{
-    // console.log(snapshot.docs);
-    snapshot.docs.forEach(doc => {
-        // console.log(doc.data());
-        renderCafe(doc);
-    });
-});
+
+
+
+
+
+// 
+// db.collection('cafes').where('city', '==', 'vrsac').get().then((snapshot)=>{
+//     db.collection('cafes').where('city', '==', 'vrsac').orderBy('name').get().then((snapshot)=>{
+//     // db.collection('cafes').orderBy('name').get().then((snapshot)=>{
+//     // db.collection('cafes').get().then((snapshot)=>{
+//     // console.log(snapshot.docs);
+//     snapshot.docs.forEach(doc => {
+//         // console.log(doc.data());
+//         renderCafe(doc);
+//     });
+// });
+
+
+
 
 // saving data
 form.addEventListener('submit', (e) => {
@@ -36,4 +59,19 @@ form.addEventListener('submit', (e) => {
     });
     form.name.value = '';
     form.city.value = '';
+})
+
+// real time listeners
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    // console.log(changes);
+    changes.forEach(change => {
+        console.log(change.doc.data());
+        if(change.type === 'added'){
+            renderCafe(change.doc);
+        } else if (change.type === 'removed'){
+            let li = cafeList.querySelector('[data-id='+change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
 })
