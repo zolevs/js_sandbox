9b2800e73205f4484bac0778a81ea72ae819d5ce
zolevs <zolevs@gmail.com>
2019-12-16 23:57:29 +0100

    refresh

--- sandbox/video4/index_091219.js
+++ sandbox/video4/index_091219.js
@@ -1,20 +1,7 @@
 { 
 
-let name = prompt('Your name');
-
-switch(name){
-    case 'zole':
-        console.log('Zole');
-        break;
-    case 'brana':
-        console.log('brana');
-        break;
-    case 'test':
-        console.log('test');
-        break;
-    default:
-        console.log('none of above');
-        
+for (let i=0; i < 19; i++){
+    console.log(i);
 }
 
 }
