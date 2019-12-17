e4f4b824ce037781f9443648dbc8c324c90fa9f5
zolevs <zolevs@gmail.com>
2019-12-17 20:57:38 +0100

    refresh

--- sandbox/video4/index_091219.js
+++ sandbox/video4/index_091219.js
@@ -1,8 +1,22 @@
 { 
 
-for (let i=0; i < 19; i++){
-    console.log(i);
-}
+    let list = 'Search this stringc baby';
+    let charToSearch = 'c';
+    for (let i = 0; i<list.length; i++){
+        console.log(list[i]);
+        if(list[i] === charToSearch){
+            console.log('Match');
+        }
+    }
+
 
 }
+
+
+
+
+
+
+
+
 
