1be7d406e0ea5c435736a845cff93ed64f2e7dbb
zolevs <zolevs@gmail.com>
2019-12-18 23:48:31 +0100

    refresh

--- sandbox/video4/index_091219.js
+++ sandbox/video4/index_091219.js
@@ -3,10 +3,11 @@
     let list = 'Search this stringc baby';
     let charToSearch = 'c';
     for (let i = 0; i<list.length; i++){
+        if(list[i] === 'e'){
+            continue;
+        }else {
         console.log(list[i]);
-        if(list[i] === charToSearch){
-            console.log('Match');
-        }
+    }
     }
 
 
