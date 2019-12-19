94f20ce5cc61d03c3293d243895b449529ba0cc9
zolevs <zolevs@gmail.com>
2019-12-19 20:59:04 +0100

    refresh

--- sandbox/video4/index_091219.js
+++ sandbox/video4/index_091219.js
@@ -1,13 +1,39 @@
 { 
 
-    let list = 'Search this stringc baby';
-    let charToSearch = 'c';
-    for (let i = 0; i<list.length; i++){
-        if(list[i] === 'e'){
-            continue;
-        }else {
-        console.log(list[i]);
+    let grades = [ 12, 34, 45, 342, 5, 11, 2, 1, 34 ];
+    grades.length = 30;
+    grades[34] = 40;
+
+
+    let found = false;
+    let search = 45;
+
+    let largest = grades[0];
+    let indexLargest = 0;
+    for (let i=0; i<grades.length; i++){
+        if(grades[i] !== undefined){
+            console.log(grades[i]);
+        }
+        if(grades[i] === search){
+            found = true;
+            console.log(grades[i] + ' at index ' + i);
+        }
+        for (let i = 0; i < grades.length; i++){
+            if (largest < grades[i]){
+                largest = grades[i];
+            }
+        }
     }
+
+    console.log("largest " + largest);
+    if(found === true){
+        console.log('Found')
+    }
+    for (let i = 0; i < grades.length; i++){
+        if(largest === grades[i]){
+            indexLargest = i;
+            console.log("index of largest " + indexLargest);
+        }
     }
 
 
