30aa201e3d9c2e6aef8fe334b78e36734abf85c2
zolevs <zolevs@gmail.com>
2019-12-23 21:24:05 +0100

    refresh

--- sandbox/reStart/prj/data/caleb/script.js
+++ sandbox/reStart/prj/data/caleb/script.js
@@ -1,15 +1,23 @@
-{
 
-    let grades = [];
+let grades = [1, 2, 3, 4, 5];
 
-    while(true){
-        let input = prompt('Add a grade');
+grades.push(100);
 
-        if(input === 'q' || input === null){
-            break;
-        }
-        grades.push(Number(input));
-        console.log(grades);
-    }
+console.log(grades.length);
 
-}
+grades.pop();
+console.log(grades);
+
+// grades.splice(2, 0, 10, 10, 10, 10, 10);
+// add afther index 2
+
+grades.splice(2, 2, 10, 10);
+
+grades.push(12,53,53,23,52,1,8,4,9,11);
+
+console.log(grades.sort(function (a,b) { return a - b}));
+
+let gradesB = [44, 66, 77, 88];
+let all = grades.concat(gradesB);
+console.log(all);
+
