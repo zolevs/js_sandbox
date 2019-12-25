d69ca64bbded83337055ea65e02fc895a8ab7187
zolevs <zolevs@gmail.com>
2019-12-26 00:16:13 +0100

    refresh

--- sandbox/reStart/prj/data/caleb/script.js
+++ sandbox/reStart/prj/data/caleb/script.js
@@ -1,10 +1,20 @@
-let grades = [1,2,4,5,6,7,3,6,2,6,7,23,55];
-grades.length = 30;
+let grades = [
+    [1,2,4,5,6,7,3,6,2,6,7,23,55],
+    [5,1,3,6,3,3,67,53,98,111,76],
+    [56,7,8,94,6,78,5,67]
+];
 
-// for (let i=0; i<grades.length; i++){
-//     console.log(grades[i]);
-// }
+outerLoop: for (let i = 0; i < grades.length; i++){
+    for(let k = 0; k < grades[i].length; k++){
+        // console.log(grades[i][k]);
+        if(grades[i][k] === 3){
+            console.log('Found');
+            continue outerLoop;
+            // break;
+        };
+        console.log('oooooo')
 
-grades.forEach(function(item, i){
-    console.log(item, i);
-});
+    };
+    console.log(' ````````` ');
+}
+
