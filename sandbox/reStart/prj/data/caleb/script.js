91ad686bfc5ac809c3680b7da72ef6b4450d310a
zolevs <zolevs@gmail.com>
2019-12-31 00:02:49 +0100

    refresh

--- sandbox/reStart/prj/data/caleb/script.js
+++ sandbox/reStart/prj/data/caleb/script.js
@@ -1,11 +1,27 @@
 {
 
-// doStuff();
+function pow(x, y){
+    let total = 1;
+    for (let i = 0; i < y; i++){
+        total *= x;
+    }
+    return total;
+}
+let collFunctions = [pow];
+console.log(collFunctions[0](3,3));
 
-var doStuff = function (){
-    console.log('Hey');
+
+let mathFunction = {
+    power: pow
 }
 
-doStuff();
+console.log(mathFunction.power(3,3));
+pow.description = "Description is here...";
 
+console.log(pow.description);
+
+function returnFunction(func){
+    return func;
+}
+console.log(returnFunction(pow(3,5)));
 }
