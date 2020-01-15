44fe37a500de9088a2e7fbba52500d7af3db4abe
zolevs <zolevs@gmail.com>
2020-01-15 19:25:22 +0100

    refresh

--- sandbox/reStart/prj/data/caleb/script.js
+++ sandbox/reStart/prj/data/caleb/script.js
@@ -1,7 +1,18 @@
 {
 
-    let myButton = document.getElementById("clickme");
-    // myButton.disabled = true;
+    // let myButton = document.getElementById("clickme");
+    // myButton.style.backgroundColor = "red";
+
+    // myButton.onclick = function(){
+    //     myButton.style.backgroundColor = "green";
+    //     myButton.disabled = true;
+    // }
+
+
+    let list = document.childNodes[1].childNodes[2].childNodes[1];
+    console.log(list);
+    console.log(list.parentElement);
+    console.log(list.nextSibling.nextSibling);
 
 }
 
