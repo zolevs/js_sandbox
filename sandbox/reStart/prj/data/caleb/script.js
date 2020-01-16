bf740f4a5eab2306ad1e464b5d2f78c12dbeffd2
zolevs <zolevs@gmail.com>
2020-01-16 23:07:53 +0100

    refresh

--- sandbox/reStart/prj/data/caleb/script.js
+++ sandbox/reStart/prj/data/caleb/script.js
@@ -9,10 +9,29 @@
     // }
 
 
-    let list = document.childNodes[1].childNodes[2].childNodes[1];
+    // let list = document.childNodes[1].childNodes[2].childNodes[1];
+    // console.log(list);
+    // console.log(list.parentElement);
+    // console.log(list.nextSibling.nextSibling);
+
+    let paragrahps = document.getElementsByTagName("p");
+    console.log(paragrahps[0].childNodes[0].nodeValue = "llamas");
+    
+    let list = document.getElementsByTagName("ol");
     console.log(list);
-    console.log(list.parentElement);
-    console.log(list.nextSibling.nextSibling);
+    let ourList = list[0];
+    console.log(ourList);
+    ourList.onmouseover = function(){
+        console.log("Mouse over");
+        ourList.childNodes[0].nodeValue = "House";
+    };
+    let button = document.getElementById("clickme");
+    button.onmouseenter = function (){
+        button.innerHTML = "Changed";
+    }
+    button.onmouseleave = function(){
+        button.innerHTML = "Hover over me";
+    }
 
 }
 
